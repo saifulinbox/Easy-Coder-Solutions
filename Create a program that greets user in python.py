6d02ae68954e7a905bfcded850831c326f90264a
saifulinbox <79947015+saifulinbox@users.noreply.github.com>
2022-10-04 21:14:06 +0530

    Add files via upload
    py

---
+++ Create a program that greets user in python.py
@@ -0,0 +1,7 @@
+'''Create a program that asks 'What is your name?' then says Hello and repeats the user's name with exclamation. Ex. if the input is 'Harriet' The output will be 'Hello Harriet!'''
+
+name = input("What is your name? ")
+#1st Formula
+print("Hello "+name+"!")
+#2nd Formula
+print(f"Hello {name}!")
